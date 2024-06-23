540a5d062174f712ae39d5d4518c7052aba0af80
Kolovratik <Kolovratik@HOME-PC>
2024-06-23 18:46:55 +0300

    Разработанный пользовательский элемент собственника

---
+++ Elements/Owner.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PR50.Contexts;
+using System.Windows.Controls;
+
+namespace PR50.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Owner.xaml
+    /// </summary>
+    public partial class Owner : UserControl
+    {
+        public Owner(OwnerContext roomOwner)
+        {
+            InitializeComponent();
+            NameOwner.Content = $"{roomOwner.Surname} {roomOwner.Name} {roomOwner.Lastname}";
+        }
+    }
+}
